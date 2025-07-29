46917c3d78170ccca2f63206402af292fc01d720
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2025-07-29 23:33:23 +0700

    updating login page

--- app/pages/login/index.vue
+++ app/pages/login/index.vue
@@ -2,6 +2,7 @@
 import { Eye, EyeOff, Loader2 } from "lucide-vue-next";
 import { AuthService } from "~/services/auth/auth.service";
 import type { LoginParams } from "~/services/auth/dto/login.dto";
+import LoginSvg from "~/assets/login.svg";
 
 const req = ref<LoginParams>({
   email: "",
@@ -32,54 +33,69 @@ async function handleLogin() {
 </script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen p-5">
-    <form @submit.prevent="handleLogin" class="w-[400px] max-w-full space-y-5">
-      <div>
-        <div class="text-2xl font-semibold">Selamat Datang 👋</div>
-        <p class="text-gray-500 text-sm">
-          Silahkan login ke akun anda untuk memulai sesi.
-        </p>
+  <div class="flex items-stretch justify-center min-h-screen p-5 relative">
+    <div class="w-1/2 lg:block hidden">
+      <div
+        class="w-full h-full bg-gray-50 rounded-lg flex items-center justify-center"
+      >
+        <img :src="LoginSvg" class="w-[400px]" alt="" />
       </div>
-      <div class="space-y-4">
-        <Input placeholder="Email" v-model="req.email" type="email"></Input>
-        <div class="relative">
-          <Input
-            placeholder="Password"
-            class="pr-10"
-            v-model="req.password"
-            :type="show_password ? 'text' : 'password'"
-          ></Input>
-          <div
-            class="absolute inset-y-0 right-3 flex items-center justify-center"
-          >
-            <button
-              type="button"
-              class="cursor-pointer"
-              @click="togglePasswordVisibility"
+    </div>
+    <div class="lg:w-1/2 w-full flex justify-center items-center">
+      <form
+        @submit.prevent="handleLogin"
+        class="max-w-full space-y-5 relative w-[400px]"
+      >
+        <div>
+          <div class="text-2xl font-semibold">Selamat Datang 👋</div>
+          <p class="text-gray-500 text-sm">
+            Silahkan login ke akun anda untuk memulai sesi.
+          </p>
+        </div>
+        <div class="space-y-4">
+          <Input placeholder="Email" v-model="req.email" type="email"></Input>
+          <div class="relative">
+            <Input
+              placeholder="Password"
+              class="pr-10"
+              v-model="req.password"
+              :type="show_password ? 'text' : 'password'"
+            ></Input>
+            <div
+              class="absolute inset-y-0 right-3 flex items-center justify-center"
             >
-              <component
-                :is="show_password ? Eye : EyeOff"
-                class="size-4"
-              ></component>
-            </button>
+              <button
+                type="button"
+                class="cursor-pointer"
+                @click="togglePasswordVisibility"
+              >
+                <component
+                  :is="show_password ? Eye : EyeOff"
+                  class="size-4"
+                ></component>
+              </button>
+            </div>
+          </div>
+
+          <div v-if="error" class="text-red-500 text-sm text-center">
+            {{ error }}
           </div>
         </div>
-
-        <div v-if="error" class="text-red-500 text-sm text-center">
-          {{ error }}
+        <Button
+          class="w-full cursor-pointer"
+          :disabled="is_submitted"
+          type="submit"
+        >
+          <Loader2
+            class="mr-1 animate-spin size-4"
+            v-if="is_submitted"
+          ></Loader2>
+          Login
+        </Button>
+        <div class="text-center text-sm text-gray-500">
+          Jika anda lupa password, silahkan hubungi admin.
         </div>
-      </div>
-      <Button
-        class="w-full cursor-pointer"
-        :disabled="is_submitted"
-        type="submit"
-      >
-        <Loader2 class="mr-1 animate-spin size-4" v-if="is_submitted"></Loader2>
-        Login
-      </Button>
-      <div class="text-center text-sm text-gray-500">
-        Jika anda lupa password, silahkan hubungi admin.
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
